2b6c4af6c47a068ebedea596a4f910ba054b33f0
spencerrojahn <spencer.rojahn@gmail.com>
2024-01-03 18:23:19 -0800

    Support sorting for /wines POST API

--- src/blueprints/apis.py
+++ src/blueprints/apis.py
@@ -1,10 +1,44 @@
+from webbrowser import get
 from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import requests
+from sqlalchemy import desc
 
 from .. import db # means from __init__.py
 from ..models import Owner, User, WineDetails, WineEntry
+
+apis = Blueprint('apis', __name__)
+
+TABLE_COLUMNS = [
+    WineEntry.cellar,               # cellar
+    WineEntry.cellar_location,      # cellar location (bin)
+    Owner.initials,                 # owner
+    WineDetails.vintage,            # vintage
+    WineDetails.varietals,          # varietals
+    WineDetails.wine_name,          # wine name
+    WineDetails.winery_name,        # winery name
+    WineDetails.winery_location,    # winery location
+    WineDetails.vineyard_location,  # vineyard location
+    WineEntry.entry_date,           # entry date
+    WineEntry.drink_date,           # drink date
+    WineEntry.drank                 # drank (true or false)
+]
+
+TABLE_COLUMN_ID_TO_DB_VALUE = {
+    'cellar-column-sort': [WineEntry, 'cellar', WineEntry.cellar],   
+    'bin-location-column-sort': [WineEntry, 'cellar_location'],   
+    'owner-column-sort': [Owner, 'initials'],   
+    'vintage-column-sort': [WineDetails, 'vintage'],   
+    'varietals-column-sort': [WineDetails, 'varietals'],  
+    'wine-name-column-sort': [WineDetails, 'wine_name'],   
+    'winery-name-column-sort': [WineDetails, 'winery_name'], 
+    'winery-location-column-sort': [WineDetails, 'winery_location'],           
+    'vineyard-location-column-sort': [WineDetails, 'vineyard_location'], 
+    'entry-date-column-sort': [WineEntry, 'entry_date'],  
+    'drink-date-column-sort': [WineEntry, 'drink_date'],   
+    'drank-column-sort': [WineEntry, 'drank'] 
+}
+
 
 def add_cell(data):
     return data if data != None else '--'
@@ -12,40 +46,74 @@ def add_cell(data):
 def clean_up_date(date):
     if date is None:
         return '--'
-    
     return date.strftime("%m/%d/%Y")
 
-apis = Blueprint('apis', __name__)
-
-@apis.route('/wines/<string:username>', methods=['GET'])
-def get_wines(username):
-
-    user = User.query.filter_by(username=username).first()
-
-    table_columns = [
-        WineEntry.cellar,               # cellar
-        WineEntry.cellar_location,      # cellar location (bin)
-        Owner.initials,                 # owner
-        WineDetails.vintage,            # vintage
-        WineDetails.varietals,          # varietals
-        WineDetails.wine_name,          # wine name
-        WineDetails.winery_name,        # winery name
-        WineDetails.winery_location,    # winery location
-        WineDetails.vineyard_location,  # vineyard location
-        WineEntry.entry_date,           # entry date
-        WineEntry.drink_date            # drink date
-    ]
-
-    response_entries = (
-        db.session.query(*table_columns)
+def wines_list_query(sortColumnId, sortOrder, filters, offset, limit):
+    query = (
+        db.session.query(*TABLE_COLUMNS)
         .join(Owner)
         .join(WineDetails)
-        .filter(WineEntry.user_id == user.id)
-        # .filter(WineDetails.varietals.ilike(f"%Cabernet%"))
-        .order_by(WineEntry.drink_date) # .order_by(WineEntry.drink_date.desc())
-        # .offset(25)  # Skip the first 25 entries (offset)
-        .limit(25)
-        .all()
+        .filter(WineEntry.user_id == current_user.id)
+    )
+
+    if filters:
+        pass    # handle later 
+
+        # query = query.filter(WineDetails.varietals.ilike(f"%cabernet%"))
+
+        
+
+    if sortColumnId:
+
+        order_by_table = TABLE_COLUMN_ID_TO_DB_VALUE[sortColumnId][0]
+        order_by_column_name = TABLE_COLUMN_ID_TO_DB_VALUE[sortColumnId][1]
+            
+        if not sortOrder:   # this will be true for asc and false for desc
+             query = query.order_by(
+                getattr(order_by_table, order_by_column_name).is_(None),
+                getattr(order_by_table, order_by_column_name).desc()
+            )
+        else:
+            query = query.order_by(
+                getattr(order_by_table, order_by_column_name).is_(None),
+                getattr(order_by_table, order_by_column_name)
+            )
+    
+
+        
+    if offset:
+        query = query.offset(offset)
+
+    if limit:
+        query = query.limit(limit) 
+
+    return query.all()
+
+
+
+
+@apis.route('/wines', methods=['POST'])
+def get_wines():
+
+    # Get the JSON payload from the request
+    body = request.get_json()
+
+    print(body)
+
+    sortColumnId = body['sort']['value'] 
+    sortOrder = body['sort']['order'] 
+
+    filters = body['filters']
+    offset = body['offset']
+    limit = body['limit']
+
+
+    query_response = wines_list_query(
+        sortColumnId, 
+        sortOrder, 
+        filters, 
+        offset, 
+        limit
     )
 
     table_date = [
@@ -60,9 +128,10 @@ def get_wines(username):
             add_cell(entry.winery_location),
             add_cell(entry.vineyard_location),
             clean_up_date(entry.entry_date),
-            clean_up_date(entry.drink_date)
+            clean_up_date(entry.drink_date),
+            entry.drank
         ]  
-        for entry in response_entries
+        for entry in query_response
     ]
 
     return jsonify(table_date)
@@ -113,3 +182,41 @@ def get_wines(username):
 #                 db.session.add(wine_details)
 #                 db.session.commit()
 
+
+
+# owner = Owner.query.filter_by(initials='SCKR').first()
+
+#     wine_details = WineDetails(
+#         winery_name='Roth Estate',
+#         winery_location='Sonoma County, CA, US',                                        # optional
+#         vineyard_location=', Sonoma Valley, Sonoma County',                     # optional
+#         wine_name='Ode to the Beast',                                                       # optional
+#         varietals='Cabernet Sauvignon', 
+#         vintage=2015,                                                                   # optional
+#         expert_rater_name='Robert Parker',                                              # optional
+#         expert_rating=97,                                                               # optional
+#         personal_rating=5                                                               # optional
+#     )
+
+#     wine_entry = WineEntry(
+#         entry_date='2023-04-01', 
+#         drink_date='2023-12-07',                # optional
+#         drank=True,                            # optional
+#         # cellar='DAD',                           # optional
+#         # cellar_location='F4-F',                 # optional
+#         purchase_price=120,                    # optional
+#         entry_description='Q2 Wine Shipment',   # optional
+#         # personal_notes='Bold and tanniny',       # optional
+#         user=current_user, 
+#         owner=owner,
+#         details = wine_details
+#     )
+
+
+
+#     # Save to the database
+#     # db.session.add(user)
+#     # db.session.add(owner)
+#     db.session.add(wine_entry)
+#     db.session.add(wine_details)
+#     db.session.commit()
--- src/static/js/wines.js
+++ src/static/js/wines.js
@@ -1,46 +1,195 @@
 
 
-function onPageLoadWineList(username) {
+function sendWinesGetRequest(sortColumnId, sortOrder, filters, offset, limit) {
     // Generate the URL for the internal route with the user parameter
-    var wines_api_url = "/apis/wines/" + username;
+    var wines_api_url = "/apis/wines";
+
+    var request_body = {
+        sort: {
+            value: sortColumnId,
+            order: sortOrder
+        },
+        filters: filters,
+        offset: offset,
+        limit: limit        
+    };
+
 
     // Use the Fetch API to make the internal GET request
-    fetch(wines_api_url)
-        .then(response => response.json()) // Parse the JSON response
-        .then(data => {
-            // Create HTML elements to display the result
-            var table_body = document.getElementById('wines-table-body');
-            console.log(data)
+    fetch(wines_api_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request_body)
+    })
+    .then(response => response.json()) // Parse the JSON response
+    .then(data => {
+        console.log(data)
+        
+        // Create HTML elements to display the result
+        var table_body = document.getElementById('wines-table-body');
+        table_body.innerHTML = '';
 
-            // Iterate through the list of lists and create rows
-            data.forEach(list => {
-                // Create a new row
-                var row = table_body.insertRow();
+        // Iterate through the list of lists and create rows
+        data.forEach(list => {
+            // Create a new row
+            var row = table_body.insertRow();
+            
+            // Set background collor if drank
+            if (list[list.length -1] === true) {
+                row.style.backgroundColor = '#eeeeee';
+                
+            }
 
-                // Iterate through the items in the list and create cells
-                list.forEach(item => {
-                    var cell = row.insertCell();
+            // Iterate through the items in the list and create cells
+            list.forEach((item, index) => {
+                var cell = row.insertCell();
+                
+                // set last row to be in the middle and use check mark
+                if (index === list.length - 1) {
+                    cell.id += 'drank-header';
+                    if (item === true) {
+                        cell.textContent = '\u2713';
+                    } 
+                } else {
+                    // else set the item normally
                     cell.textContent = item;
-                });
+                }
             });
-
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('Error:', error);
-
-            // Display an error message
-            var resultContainer = document.getElementById('wines-table-body');
         });
+
+    })
+    .catch(error => {
+        // Handle errors
+        console.error('Error:', error);
+
+    });
+}
+
+
+function sortingClick(link, sortingSpan)  {
+
+    // 1 = unclicked
+    // 2 = ascending
+    // 3 = descending
+    var currentState;
+    if (link.style.color === 'black') {
+        currentState = 1;
+    } else {
+        if (sortingSpan.textContent === '\u2191') {
+            currentState = 2;
+        } else {
+            currentState = 3;
+        }
+    }
+
+    // Iterate through the elements and set the style
+    var elementsWithClass = document.querySelectorAll('a.column-header');
+    elementsWithClass.forEach(function(element) {
+        element.style.color = 'black';
+    });
+
+    var sortingSymbols = document.querySelectorAll('span.sorting-symbol');
+    sortingSymbols.forEach(function(symbol) {
+        symbol.textContent = '-';
+    });
+
+    switch (currentState) {
+        case 1:
+            // move to state 2 ascending
+            sortingSpan.textContent = '\u2191';
+            link.style.color = '#797979';
+
+            sendWinesGetRequest(link.id, true, null, 0, 25);
+            
+            // send get request for ascending for this given column
+
+            break;
+        case 2:
+            // move to state 3 descending
+            sortingSpan.textContent = '\u2193';
+            link.style.color = '#797979';
+
+            sendWinesGetRequest(link.id, false, null, 0, 25);
+
+            // send get request for desc on this given column
+
+            break;
+        case 3:
+            // move to state 1 unclicked
+            sortingSpan.textContent = '\u2191';
+            link.style.color = '#797979';
+
+            sendWinesGetRequest(link.id, true, null, 0, 25);
+
+            break;
+        default:
+            break;
+    }
+
 }
 
 
 function onPageLoad() {
-    // Get the user information from the data attribute of the link or any other source
-    var username = document.getElementById('header-username').textContent;
+    
+    // Get the table header element
+    var tableHead = document.querySelector('#wines-table-head');
+
+    // Define an array of column names (including names with spaces)
+    var columnNames = ['Cellar', 'Bin Location', 'Owner', 'Vintage', 'Varietals', 
+                        'Wine Name', 'Winery Name', 'Winery Location',
+                        'Vineyard Location', 'Entry Date', 'Drink Date', 'Drank'];
+
+    // Create th elements and append them to the table header
+    columnNames.forEach(function(columnName) {
+        var th = document.createElement('th');
+
+        // Replace spaces with dashes in the column name for the id attribute
+        var columnNameId = columnName.toLowerCase().replace(/\s+/g, '-');
+
+        // Create link element
+        var link = document.createElement('a');
+        link.style.color = 'black';
+        link.id = columnNameId + '-column-sort';
+        link.className = 'column-header';
+
+        // Create span elements
+        var spanColumnName = document.createElement('span');
+        spanColumnName.textContent = columnName;
+
+        var sortingSpan = document.createElement('span');
+        sortingSpan.id = columnNameId + '-sort-symbol'
+        sortingSpan.className = 'sorting-symbol';
+        sortingSpan.textContent = '-';
+
+        // // Initial sorting when created
+        if (columnName === 'Drink Date') {
+            link.style.color = '#797979';
+            sortingSpan.textContent = '\u2191';
+        }
+
+        // Append span elements to link
+        link.appendChild(spanColumnName);
+        link.appendChild(sortingSpan);
+
+        // Add onclick function to the link
+        link.onclick = function() {
+            sortingClick(link, sortingSpan)
+        }
+
+        // Append link to th
+        th.appendChild(link);
+
+        // Append th to table header
+        tableHead.appendChild(th);
+    });
+
+    // SEND INTITIAL with 'Drink Date' sorting
 
     // Call the function to make the internal API request on page load
-    onPageLoadWineList(username);
+    sendWinesGetRequest('drink-date-column-sort', true, null, 0, 25);
+    // sendWinesGetRequest(null, null, null, 0, 25);
 }
 
 
